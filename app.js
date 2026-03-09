914fb5be24b76e7f3816f03ce7ba9c1340a9c188
DavidPineda02 <davxpa.02@gmail.com>
2026-03-09 17:57:29 -0500

    config: agregar middleware y rutas PQRS

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import pqrsRoutes from './src/routes/pqrs.routes.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use("/pqrs", pqrsRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en: http://localhost:${PORT}`);
 });
